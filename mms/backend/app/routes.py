4ecdc3d19746add1c87297badb13dbb8b9510bfd
sandile <skhendle@gmail.com>
2022-03-29 05:44:03 +0200

    added parameters to routes

--- mms/backend/app/routes.py
+++ mms/backend/app/routes.py
@@ -2,8 +2,7 @@
 from datetime import datetime as dt
 
 from flask import current_app as app
-from flask import make_response, redirect, render_template, request, url_for
-
+from flask import make_response, redirect, render_template, request, url_for, jsonify
 from .models import MMSDATA, db
 
 # TODO: Add route paramaters
@@ -16,16 +15,23 @@ def get_pond_records():
     return {'username':username,'email':email}
 
 
-@app.route("/post", methods=["GET"])
+@app.route("/pond_photo/<pond_id>", methods=["GET"])
+def get_pond_photo():
+    """Create a user via query string parameters."""
+    username = request.args.get("user")
+    email = request.args.get("email")
+    return {'username':username,'email':email}
+
+@app.route("/post", methods=["POST"])
 def add_pond_records():
     """Create a user via query string parameters."""
     username = request.args.get("user")
     email = request.args.get("email")
     return {'username':username,'email':email}
 
-@app.route("/rate", methods=["Post"])
-def rate_pond():
+@app.route("/rate/<data_id>", methods=["GET"])
+def rate_pond(data_id):
     """Create a user via query string parameters."""
     username = request.args.get("user")
     email = request.args.get("email")
-    return {'username':username,'email':email}
+    return {'username':username,'email':email, 'id':data_id}

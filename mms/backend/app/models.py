ddc7a90e02f60f99e2bb68373d70968d7e873f30
sandile <skhendle@gmail.com>
2022-03-29 16:01:30 +0200

    completed basic API functionality

--- mms/backend/app/models.py
+++ mms/backend/app/models.py
@@ -2,16 +2,17 @@
 from . import db
 
 
-class MMSDATA(db.Model):
+class PondEntries(db.Model):
     """Data model"""
 
-    __tablename__ = "mms_data"
+    __tablename__ = "pond_entries"
     id = db.Column(db.Integer, primary_key=True)
     created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
     description = db.Column(db.String(100), index=False, unique=False, nullable=True)
     rating = db.Column(db.String(80), index=True, unique=False, nullable=True)
     photo = db.Column(db.LargeBinary(length=None), index=False, unique=False, nullable=True)
+    photo_type = db.Column(db.String(80), index=True, unique=False, nullable=True)
 
 
     def __repr__(self):
-        return "<User {}>".format(self.username)
+        return "<Pond {}>".format(self.description)
--- mms/backend/app/routes.py
+++ mms/backend/app/routes.py
@@ -1,27 +1,30 @@
 """Application routes."""
 from datetime import datetime as dt
-import os
-from urllib import response
+from encodings import utf_8
+import json, io, os
 from flask import current_app as app
-from flask import make_response, request, url_for, jsonify
-from .models import MMSDATA, db
-
+from flask import make_response, request, jsonify, send_file
+from .models import PondEntries, db
+import PIL.Image as Image
 # TODO: Add route paramaters
 
-
-@app.route("/", methods=["GET"])
+@app.route("/get_pond_records", methods=["GET"])
 def get_pond_records():
     # Get all pond records available in the database
     try:
-        result = MMSDATA.query.all()
+        result = PondEntries.query.all()
         response = []
+        
         for item in result:
-            response.add(
+            # print(type(item.photo))
+            
+            response.append(
                 {
-                    'id': item('id'),
-                    'created': item('created'),
-                    'description': item('description'),
-                    'rating': item('rating')
+                    'id': item.id,
+                    'created': item.created,
+                    'description': item.description,
+                    'rating': item.rating,
+                    # 'photo': item.photo.decode("utf-8") 
                 }
             )
         return make_response(jsonify(response),200)
@@ -29,14 +32,19 @@ def get_pond_records():
         return make_response(str(e), 400)
 
 
-
-@app.route("/pond_photo/<pond_id>", methods=["GET"])
-def get_pond_photo(pond_id:int):
-    # Return blob related to pond_id
+# request shouild be 'pond_photo?pond_id=1'
+@app.route("/pond_photo", methods=["GET"])
+def get_pond_photo():
+    args = request.args
     try:
-        pond_id = int(pond_id)
-        return {'pond_id':pond_id}
+        pond_id = args.get("pond_id", default="", type=str)
+        result = PondEntries.query.get(int(pond_id))
+        filename =  '/app/static/'+str(result.id)+"."+str(result.photo_type)
+        image = Image.open(io.BytesIO(result.photo))
+        image.save(str(os.getcwd())+filename)
+        return send_file(str(os.getcwd())+filename, mimetype='image/png')
     except Exception as e:
+        print(str(e))
         return make_response('Invalid Request', 400)
 
 
@@ -45,16 +53,41 @@ def get_pond_photo(pond_id:int):
 @app.route("/add_pond", methods=["POST"])
 def add_pond_records():
     """Create a user via query string parameters."""
-    username = request.args.get("user")
-    email = request.args.get("email")
-    return {'username':username,'email':email}
+
+    img = request.files['file']
+    record = PondEntries(
+        created = dt.now(),
+        description = request.form['description'],
+        photo = img.read(),
+        photo_type = img.filename.split('.')[1]
+    )
+    try:
+        db.session.add(record)
+        db.session.commit()
+        return make_response('Record Added', 201)
+    except Exception as e:
+        return make_response('Invalid Request', 400)
 
 
  # Post to method data == {'id':##,'rating':###}
 @app.route("/rate_pond", methods=["POST"])
 def rate_pond():
     """Create a user via query string parameters."""
-    data = request.data
-    print(data)
+    data = json.loads(request.data)
 
-    return data
+    result = PondEntries.query.get(data['id'])
+    if result:
+        try:
+            result.rating = data['rating']
+            db.session.add(result)
+            db.session.commit()
+            return make_response(jsonify({
+                    'id': result.id,
+                    'created': result.created,
+                    'description': result.description,
+                    'rating': result.rating
+                }), 200)
+        except Exception as e:
+            return make_response('Invalid Request', 401)
+
+    return make_response('Invalid Pond Id', 400)

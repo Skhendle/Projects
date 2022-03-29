7d93d34509a0bb4342cccb7545516af986dda799
sandile <skhendle@gmail.com>
2022-03-29 07:14:35 +0200

    completed get pond records

--- mms/backend/app/routes.py
+++ mms/backend/app/routes.py
@@ -1,18 +1,32 @@
 """Application routes."""
 from datetime import datetime as dt
-
+import os
+from urllib import response
 from flask import current_app as app
-from flask import make_response, redirect, render_template, request, url_for, jsonify
+from flask import make_response, request, url_for, jsonify
 from .models import MMSDATA, db
 
 # TODO: Add route paramaters
 
+
 @app.route("/", methods=["GET"])
 def get_pond_records():
     # Get all pond records available in the database
-    username = request.args.get("user")
-    email = request.args.get("email")
-    return {'username':username,'email':email}
+    try:
+        result = MMSDATA.query.all()
+        response = []
+        for item in result:
+            response.add(
+                {
+                    'id': item('id'),
+                    'created': item('created'),
+                    'description': item('description'),
+                    'rating': item('rating')
+                }
+            )
+        return make_response(jsonify(response),200)
+    except Exception as e:
+        return make_response(str(e), 400)
 
 
 
@@ -28,7 +42,7 @@ def get_pond_photo(pond_id:int):
 
 # upload pond condition
 # upload picture and description
-@app.route("/post", methods=["POST"])
+@app.route("/add_pond", methods=["POST"])
 def add_pond_records():
     """Create a user via query string parameters."""
     username = request.args.get("user")
@@ -37,7 +51,7 @@ def add_pond_records():
 
 
  # Post to method data == {'id':##,'rating':###}
-@app.route("/rate", methods=["POST"])
+@app.route("/rate_pond", methods=["POST"])
 def rate_pond():
     """Create a user via query string parameters."""
     data = request.data

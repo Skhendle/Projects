135a2ccca66ff50ca6ff50a8d9d767283cc73cdd
Sandile Khendle <skhendle@gmail.com>
2022-03-31 23:54:41 +0200

    updated pond view page

--- mms/frontend/pond/lib/view_ponds/view_ponds.dart
+++ mms/frontend/pond/lib/view_ponds/view_ponds.dart
@@ -13,42 +13,34 @@ class _ViewPondsWidgetState extends State<ViewPondsWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text(
+          "View Ponds",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
       key: scaffoldKey,
       backgroundColor: const Color(0xCF3D77A8),
       body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
-                child: Container(
-                  width: double.infinity,
-                  height: 80,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFEEEEEE),
-                  ),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back_outlined,
-                      color: Colors.black,
-                      size: 50,
-                    ),
-                    onPressed: () async {
-                      Navigator.pop(context);
-                    },
-                  ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsetsDirectional.fromSTEB(10, 10, 15, 0),
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xCF3D77A8).withOpacity(0.5),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(50, 10, 50, 0),
-                child: Container(
-                  width: double.infinity,
-                  height: 600,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFEEEEEE),
-                  ),
+                child: Scrollbar(
+                  isAlwaysShown: true,
                   child: ListView(
                     padding: EdgeInsets.zero,
                     scrollDirection: Axis.vertical,
@@ -113,12 +105,72 @@ class _ViewPondsWidgetState extends State<ViewPondsWidget> {
                           elevation: 10,
                         ),
                       ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 5),
+                        child: Card(
+                          child: SizedBox(
+                            height: 125,
+                            width: 100,
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.lightGreenAccent,
+                          elevation: 10,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 5),
+                        child: Card(
+                          child: SizedBox(
+                            height: 125,
+                            width: 100,
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.lightGreenAccent,
+                          elevation: 10,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 5),
+                        child: Card(
+                          child: SizedBox(
+                            height: 125,
+                            width: 100,
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.lightGreenAccent,
+                          elevation: 10,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 5),
+                        child: Card(
+                          child: SizedBox(
+                            height: 125,
+                            width: 100,
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.lightGreenAccent,
+                          elevation: 10,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 5, 15, 5),
+                        child: Card(
+                          child: SizedBox(
+                            height: 125,
+                            width: 100,
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.lightGreenAccent,
+                          elevation: 10,
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );

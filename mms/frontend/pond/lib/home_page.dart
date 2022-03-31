d7aacc7bffe22a48e66bec4d20501a92b1838180
Sandile Khendle <skhendle@gmail.com>
2022-03-31 12:10:21 +0200

    completed home page

--- mms/frontend/pond/lib/home_page.dart
+++ mms/frontend/pond/lib/home_page.dart
@@ -25,44 +25,80 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
-              child: ElevatedButton.icon(
-                onPressed: () async {
-                  await availableCameras().then(
-                    (value) => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => UploadPageWidget(
-                          cameras: value,
+          Expanded(
+              flex: 3,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/pond.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              )),
+          Expanded(
+            flex: 1,
+            child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(30, 10, 30, 10),
+                child: ElevatedButton.icon(
+                  onPressed: () async {
+                    await availableCameras().then(
+                      (value) => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UploadPageWidget(
+                            cameras: value,
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.upload),
-                label: const Text('Upload Pond'),
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 15),
-                ),
-              )),
-          Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
-              child: ElevatedButton.icon(
-                onPressed: () async {
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ViewPondsWidget(),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.image_search),
-                label: const Text('View Pond States'),
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 15),
-                ),
-              )),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.upload,
+                    size: 30,
+                  ),
+                  label: const Text(
+                    'Upload Pond',
+                    style: TextStyle(
+                        fontFamily: 'lato',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 15),
+                  ),
+                )),
+          ),
+          Expanded(
+            child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(30, 20, 30, 20),
+                child: ElevatedButton.icon(
+                  onPressed: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ViewPondsWidget(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.image_search,
+                    size: 30,
+                  ),
+                  label: const Text(
+                    'View Pond States',
+                    style: TextStyle(
+                        fontFamily: 'lato',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 15),
+                  ),
+                )),
+          ),
         ],
       )),
     );

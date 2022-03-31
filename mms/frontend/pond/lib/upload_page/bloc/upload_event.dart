25f12eced72b8b69657fad114b08dbcf081ee908
Sandile Khendle <skhendle@gmail.com>
2022-04-01 00:15:00 +0200

    setting up ents for upload page

--- mms/frontend/pond/lib/upload_page/bloc/upload_event.dart
+++ mms/frontend/pond/lib/upload_page/bloc/upload_event.dart
@@ -1,4 +1,21 @@
 part of 'upload_bloc.dart';
 
-@immutable
-abstract class UploadEvent {}
+
+
+abstract class UploadEvent extends Equatable {
+  const UploadEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class UpdatedPondDescription extends UploadEvent {
+  const UpdatedPondDescription({required this.description});
+
+  final String description;
+
+  @override
+  List<Object> get props => [description];
+}
+
+class UploadPond extends UploadEvent {}

dcb8a9d5e7ec0664f320325227996a47ddc49cf4
Sandile Khendle <skhendle@gmail.com>
2022-04-05 09:26:54 +0200

    completed rating bloc

--- mms/frontend/pond/lib/view_ponds/bloc/view_ponds_event.dart
+++ mms/frontend/pond/lib/view_ponds/bloc/view_ponds_event.dart
@@ -1,4 +1,26 @@
 part of 'view_ponds_bloc.dart';
 
-@immutable
-abstract class ViewPondsEvent {}
+abstract class RatePondEvent extends Equatable {
+  const RatePondEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class UodatePondRating extends RatePondEvent {
+  const UodatePondRating({required this.rating});
+  final String rating;
+
+
+  @override
+  List<Object> get props => [rating];
+}
+
+class RatePond extends RatePondEvent {
+  const RatePond({required this.pondId});
+
+    final int pondId;
+
+  @override
+  List<Object> get props => [pondId];
+}
--- mms/frontend/pond/lib/view_ponds/bloc/view_ponds_state.dart
+++ mms/frontend/pond/lib/view_ponds/bloc/view_ponds_state.dart
@@ -1,6 +1,35 @@
 part of 'view_ponds_bloc.dart';
 
-@immutable
-abstract class ViewPondsState {}
+class RatePondsState extends Equatable {
+  const RatePondsState(
+      {this.rating = const Name.pure(),
 
-class ViewPondsInitial extends ViewPondsState {}
+      this.status = FormzStatus.pure});
+
+  final Name rating;
+
+  final FormzStatus status;
+  RatePondsState copyWith({
+    Name? rating,
+    FormzStatus? status,
+  }) {
+    return RatePondsState(
+      rating: rating ?? this.rating,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object> get props => [rating, status];
+}
+
+class RatingResponse extends RatePondsState {
+  final bool success;
+  final String message;
+
+  const RatingResponse(this.success, this.message) : super();
+}
+
+class RatingPond extends RatePondsState {}
+
+class RatingInitial extends RatePondsState {}

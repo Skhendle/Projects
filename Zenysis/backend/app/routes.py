d8126497285116245b8502ea965b7d49c6f7f49c
sandile <skhendle@gmail.com>
2022-04-07 07:48:30 +0200

    setting up routes

--- Zenysis/backend/app/routes.py
+++ Zenysis/backend/app/routes.py
@@ -5,16 +5,31 @@ from .models import CountriesCovidData, db
 from .data_caching import DataRepository
 
 
-
+# Return The list of countries with number of data points available
 @app.route("/")
-def available_places():
-    pass
+def home():
+    return make_response('Welcome', 201)
 
+# TODO:
+# Extend route to allow countryName input
+# Get country base on name input
 @app.route("/get_country", methods=["GET"])
 def get_country_data():
     data_repo = DataRepository()
     if data_repo.checkLocalData():
             return make_response('Data is cached', 201)
+
+    data_repo.fetchData()
+    return make_response('Data is not cached', 400)
+
+# TODO:
+# 1. Get data for the selected place
+# Based on country and place name 
+@app.route("/get_place", methods=["GET"])
+def available_places():
+    data_repo = DataRepository()
+    if data_repo.checkLocalData():
+        return make_response('Data is cached', 201)
 
     
     data_repo.fetchData()

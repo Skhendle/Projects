daefc2b6c696ff0305212f6a6704a16980714b22
Sandile Khendle <skhendle@gmail.com>
2022-04-08 12:53:51 +0200

    separated tasks functionality and compoments

--- learning_react/react-task-tracker/src/components/Task.js
+++ learning_react/react-task-tracker/src/components/Task.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {FaTimes} from 'react-icons/fa'
 
-const Task = ({task}) => {
+const Task = ({task, onDelete}) => {
   return (
     <div className='task'>
         <h3>{task.day} <FaTimes style={{
             color: 'red',
             cursor: 'pointer'
-        }}/></h3>
+        }}
+        onClick={()=> onDelete(task.id)}
+        /></h3>
         <p>{task.text}</p>
 
     </div>
--- learning_react/react-task-tracker/src/components/Tasks.js
+++ learning_react/react-task-tracker/src/components/Tasks.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Task from './Task'
+import PropTypes from 'prop-types'
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, onDelete}) => {
 
   return (
     <>
         {tasks.map((task) => (
-            <Task key={task.id} task={task}/>
+            <Task key={task.id} task={task} onDelete={onDelete}/>
         ))}
     </>
   )
 }
 
+Tasks.prototype = {
+  onDelete: PropTypes.func
+}
 export default Tasks

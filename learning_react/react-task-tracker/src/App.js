c4eedce3702ef076fe1675540eabf22e6c5162fa
Sandile Khendle <skhendle@gmail.com>
2022-04-08 12:13:09 +0200

    removed default react setup

--- learning_react/react-task-tracker/src/App.js
+++ learning_react/react-task-tracker/src/App.js
@@ -1,12 +1,8 @@
-
-
+import Header from "./components/Header";
 function App() {
-  const x = Math.floor(Math.random()*10);
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      <h2>Do you think this journey will be worth a while? {x%2===0? 'No': 'Yes'}</h2>
-      
+      <Header/>
     </div>
   );
 }

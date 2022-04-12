42e14dd68ed0704da0c24935187926e634264fe8
Sandile Khendle <skhendle@gmail.com>
2022-04-12 11:37:31 +0200

    clean up react&node modules

---
+++ Zenysis/frontend/covid-data/src/components/Dropdown.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+const Dropdown = () => {
+    const [selectedCountry, setTasks] = useState()
+  return (
+    <div>Dropdown</div>
+  )
+}
+
+export default Dropdown

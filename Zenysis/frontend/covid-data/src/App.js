0fadda3c362fc165899cfb8951c090ac6349607a
Sandile Khendle <skhendle@gmail.com>
2022-04-12 19:34:05 +0200

    completed data change on dropdown change

--- Zenysis/frontend/covid-data/src/App.js
+++ Zenysis/frontend/covid-data/src/App.js
@@ -5,53 +5,69 @@ import { SelectedCountry } from "./components/SelectedCountry";
 import axios from 'axios'
 
 function App() {
-  const [countryId, setCountryId] = useState(1)
+  const [countryId, setCountryId] = useState(-1)
   const [countries, setCountries] = useState([])
-  const [countryData, setCountryData] = useState(null)
+
 
   useEffect(()=>{
-    if (countries.length === 0){
-      fetchCountryList();
-    }else{
-      fetchCountryData()
+    console.log(countryId)
+    if(countryId === -1){
+        axios.get('https://b40d-197-229-148-158.ngrok.io/')
+      .then(res =>{
+          return res.data
+      }).then(data => {
+        setCountries(data)
+        setCountryId(data[0].id)
+      }).catch(  err => 
+          console.log(err)
+      )
+
+  
     }
+
     
 }, [countryId])
 
-const fetchCountryList = () => {
-  axios.get('https://b40d-197-229-148-158.ngrok.io/')
-    .then(res =>{
-        return res.data
-    }).then(data => {
-      setCountries(data)
-      setCountryId(data[0].id)
-    }).catch(  err => 
-        console.log(err)
-    )
-}
 
-const fetchCountryData = () => {
-  console.log(`country id ${countryId}`)
-  axios.get(`https://b40d-197-229-148-158.ngrok.io/get_country?id=${countryId}`)
-  .then(res =>{
-      setCountryData(res.data)
-  }).catch(  err => 
-      console.log(err)
-    )}
+
+// const fetchCountryList = () => {
+//   axios.get('https://b40d-197-229-148-158.ngrok.io/')
+//     .then(res =>{
+//         return res.data
+//     }).then(data => {
+//       setCountries(data)
+//       setCountryId(data[0].id)
+//     }).catch(  err => 
+//         console.log(err)
+//     )
+// }
+
+// const fetchCountryData = () => {
+  // console.log(`country id ${countryId}`)
+  // axios.get(`https://b40d-197-229-148-158.ngrok.io/get_country?id=${countryId}`)
+  // .then(res =>{
+  //     setCountryData(res.data)
+  // }).catch(  err => 
+  //     console.log(err)
+  //   )
+// }
 
   const onChangeCountry = (cId) =>{
-    console.log('called here with id ' + cId)
    setCountryId(cId)
   }
 
 
-  return (
-    <div className="container">
-       <Dropdown countries = {countries} onChangeCountry={onChangeCountry}/>
-       <h3>{countryId}</h3>
-       <SelectedCountry countryData={countryData}/>
 
-    </div>
+  return (
+    
+       countryId === -1 ? null : (
+          <div className="container">
+          <Dropdown countries = {countries} onChangeCountry={onChangeCountry}/>
+          <h3>{countryId}</h3>
+          <SelectedCountry countryId={countryId} /> 
+          </div>
+         )
+
   );
 }
 
--- Zenysis/frontend/covid-data/src/components/SelectedCountry.js
+++ Zenysis/frontend/covid-data/src/components/SelectedCountry.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState} from "react";
+import axios from 'axios'
 
-export const SelectedCountry = ({countryData}) => {
-  return (
-    <div className='task'>
-        <h1>{countryData.capital_city}</h1>
-        <h1>{countryData.abbreviation}</h1>
-        <h1>{countryData.capital_city}</h1>
-        <h1>{countryData.confirmed}</h1>
-        <h1>{countryData.continent}</h1>
-        <h1>{countryData.country}</h1>
-        <h1>{countryData.location}</h1>
-        <h1>{countryData.sq_km_area}</h1>
-        <h1>{countryData.updated}</h1>
 
-    </div>
+
+export const SelectedCountry = (props) => {
+    const [countryData, setCountryData] = useState(null)
+
+    useEffect(()=>{
+        
+        axios.get(`https://b40d-197-229-148-158.ngrok.io/get_country?id=${props.countryId}`)
+        .then(res =>{
+            console.log(res.data)
+            setCountryData(res.data)
+        }).catch(  err => 
+            console.log(err)
+            )
+        },[props.countryId])
+
+    return (
+        countryData === null ? null : (
+            <div className='task'>
+                <h1><span style="color:blue">{countryData.country}</span></h1>
+            <h1>Capital City :<br/>{countryData.capital_city}</h1>
+            <h1>Abbreviation :<br/>{countryData.abbreviation}</h1>
+            <h1>Continent :<br/>{countryData.continent}</h1>
+            <h1>location :<br/>{countryData.location}</h1>
+            {/* <h1>Capital City :<br/>{countryData.continent}</h1>
+            <h1>Capital City :<br/>{countryData.country}</h1>
+            <h1>Capital City :<br/>{countryData.location}</h1>
+            <h1>Capital City :<br/>{countryData.sq_km_area}</h1>
+            <h1>Capital City :<br/>{countryData.updated}</h1> */}
+    
+        </div>
+           )
+
   )
 }
